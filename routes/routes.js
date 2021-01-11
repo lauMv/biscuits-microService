e26dc0465812fc2a681317d1ebd93bb1beb8ea01
laura Meneses <lmeneses58@gmail.com>
2021-01-11 18:12:11 -0400

    change route

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();  
 const biscuitService = require('./microservice/biscuits')
   
-router.use('/biscuits', biscuitService);
+router.use('/cakes', biscuitService);
 
 module.exports = router;
